ff27fedc3d153050ccf66e93bb60b99fbb9f3a15
osnailcyargta-ctrl <osnail.cyargta@gmail.com>
2026-06-14 15:24:47 +0700

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
+}
+dependencyResolutionManagement {
+    repositories { google(); mavenCentral() }
+}
+rootProject.name = "PineLauncher"
+include(":app")
